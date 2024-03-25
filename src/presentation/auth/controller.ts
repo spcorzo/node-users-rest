9704f54a2c2e2e122f473775b963569874deab23
Santiago P. Corzo <santiago.p.corzo@hotmail.com>
2024-03-25 17:10:38 -0500

    Implement register user repository in controller

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,15 +1,20 @@
 import { Request, Response } from "express";
 import { RegisterUserDto } from "../../domain/dtos";
+import { AuthRepository } from "../../domain/repositories";
 
 export class AuthController {
-  constructor() {}
+  constructor(
+    private readonly authRepository: AuthRepository,
+  ) {}
 
   registerUser = (req: Request, res: Response) => {
     const [error, registerUserDto] = RegisterUserDto.create(req.body);
 
     if (error) return res.status(400).json({message: error});
 
-    res.json(registerUserDto);
+    this.authRepository.register(registerUserDto!)
+      .then(user => res.json(user))
+      .catch(error => res.status(500).json(error));
   };
 
   loginUser = (req: Request, res: Response) => {
--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { AuthController } from "./controller";
+import { AuthRepositoryImplementation } from "../../infraestructure/respositories";
+import { AuthDatasourceImplementation } from "../../infraestructure/datasources";
 
 export class AuthRoutes {
   static get routes(): Router {
     const router = Router();
-    const controller = new AuthController();
+
+    const datasource = new AuthDatasourceImplementation();
+    const authRepository = new AuthRepositoryImplementation(datasource);
+    
+    const controller = new AuthController(authRepository);
 
     router.post("/login", controller.loginUser);
 
