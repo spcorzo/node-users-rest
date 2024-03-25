db3807ce1ad57b9d468c4e078e7c76f7f8ba489b
Santiago P. Corzo <santiago.p.corzo@hotmail.com>
2024-03-25 18:35:57 -0500

    Add handle error funciton to return the error name and error code

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,11 +1,22 @@
 import { Request, Response } from "express";
 import { RegisterUserDto } from "../../domain/dtos";
 import { AuthRepository } from "../../domain/repositories";
+import { CustomError } from "../../domain/errors";
 
 export class AuthController {
   constructor(
     private readonly authRepository: AuthRepository,
   ) {}
+
+  private handleError = (error: unknown, res: Response) => {
+    if ( error instanceof CustomError ) {
+      return res.status(error.statusCode).json({error: error.message});
+    }
+
+    console.log(error);
+
+    return res.status(500).json({error: "Internal server error."});
+  }
 
   registerUser = (req: Request, res: Response) => {
     const [error, registerUserDto] = RegisterUserDto.create(req.body);
@@ -14,7 +25,7 @@ export class AuthController {
 
     this.authRepository.register(registerUserDto!)
       .then(user => res.json(user))
-      .catch(error => res.status(500).json(error));
+      .catch(error => this.handleError(error, res));
   };
 
   loginUser = (req: Request, res: Response) => {
